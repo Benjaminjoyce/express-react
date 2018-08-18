d6fa7ed14c311f04225a2d94fc5722909279fc27
Madeliene Stiller <madeliene@Madelienes-MacBook-Pro.local>
2018-08-18 19:47:59 +1000

    Authentication process complete

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require ("express");
-require('./services/passport');
+const mongoose = require ("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require('./config/keys');
+require('./models/user'); 
+require('./services/passport'); 
 
-//has to use requre import not working syntax error??
-// import {googleClientID} from "./config/keys";
-// import {googleClientSecret} from "./config/keys";
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require('./routes/authRoutes')(app);
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
@@ -17,6 +28,9 @@ app.listen(PORT);
 
 
 
-
 //https://obscure-dusk-29323.herokuapp.com/
 //https://git.heroku.com/obscure-dusk-29323.git
+
+//has to use requre import not working syntax error??
+// import {googleClientID} from "./config/keys";
+// import {googleClientSecret} from "./config/keys";
