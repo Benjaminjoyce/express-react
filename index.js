937361e40cfac06bb6f77c8a09721c9c75046d0c
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-08-19 12:17:20 +1000

    make better

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require ("express");
-const mongoose = require ("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require('./config/keys');
-require('./models/user'); 
-require('./services/passport'); 
+require('./models/user');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
