6df888160907baa7a0bc2f91dc91ff812533616e
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-08-19 12:24:08 +1000

    make better

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,6 @@
-
-module.exports ={
+module.exports = {
     googleClientID: '574471941488-9fs4acjk795a9qf7uit48gp3jtfv0b80.apps.googleusercontent.com',
     googleClientSecret: 'mmOTKBpt1754tfqslJ7nEjlS',
     mongoURI: 'mongodb://benjaminjoyce:simpsons9711@ds125892.mlab.com:25892/emaily-dev',
     cookieKey: 'hsadasdfjkljhu'
 };
-
