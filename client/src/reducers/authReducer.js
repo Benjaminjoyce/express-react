c251bccbbfbe82c3a1d209aac32cb78347f10855
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-08-25 18:33:18 +1000

    make better

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,10 @@
+//start by assuming state will be undefined and instead set it as an empty object.
+//switch over actions with action.type being sure to return state everytime.
+//return a NEW state everytime. state must not be mutated!!!!
+
+
+export default function(state = {}, action) {
+    switch (action.type) {
+        default: return state;
+    }
+}
