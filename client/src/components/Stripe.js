3ebcacaa303d8340e0cf465ba5974ad0e32e873a
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-08-28 14:07:58 +1000

    make better

--- client/src/components/Stripe.js
+++ client/src/components/Stripe.js
@@ -9,7 +9,7 @@ class Payments extends Component {
       <StripeCheckout
         amount={500}
         token={token => this.props.handleToken(token)}
-        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+        stripeKey={process.env.STRIPE_PUBLISHABLE_KEY}
         description="$5 for 5 email credits"
       >
         {' '}
