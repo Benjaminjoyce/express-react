73b6526959c3a8caf918000cabc1947bf83b307f
Benjaminjoyce <40448456+Benjaminjoyce@users.noreply.github.com>
2018-09-06 13:47:50 +1000

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    googleClientID: '574471941488-9fs4acjk795a9qf7uit48gp3jtfv0b80.apps.googleusercontent.com',
-    googleClientSecret: 'mmOTKBpt1754tfqslJ7nEjlS',
-    mongoURI: 'mongodb://benjaminjoyce:simpsons9711@ds125892.mlab.com:25892/emaily-dev',
-    cookieKey: 'hsadasdfjkljhu',
-    stripePublishableKey: 'pk_test_RP3Ez2kGTKeRjvbooQB6XXBM',
-    stripeSecretKey: 'sk_test_iF3ADoAh1D7kQuYX3DptJ575',
-    sendGridKey: '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'
-};
